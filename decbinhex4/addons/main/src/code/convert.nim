95f68e3c29f6784d04887062f7cf778ba7ad96f4
insomniac_lemon <insomniac.lemon@gmail.com>
2024-07-30 07:17:41 -0400

    improve icon (#2)
    * improve icon
    
    Outline, looks better at low resolution
    
    * Finish node type changes
    
    Nodes properly stored in class instead of string for better readability, code condensing

--- decbinhex4/addons/main/src/code/convert.nim
+++ decbinhex4/addons/main/src/code/convert.nim
@@ -2,28 +2,24 @@ import math, strutils
 import std/times #benchmark only
 
 # the asterisk is so it can be called from another file when imported
-proc convert_from_decimal*(input_decimal: string): string =
-  var result_binary: string = "reset binary result"
-  var result_hex: string = "reset hex result"
-  if input_decimal.len < 20:
-    result_binary = strip((input_decimal.parseInt).toBin(63), trailing = false, chars = {'0'})
+proc convert_from_decimal*(input_decimal: string):string =
+  var result_binary: string = "reset binary result"; var result_hex: string = "reset hex result"
+  if input_decimal.len < 20: result_binary = strip((input_decimal.parseInt).toBin(63), trailing = false, chars = {'0'})
   else: result_binary = "Output binary too large!"
   result_hex = strip(input_decimal.parseInt.toHex, trailing = false, chars = {'0'})
   return("Binary is: " & result_binary & "\nHexadecimal is: " & result_hex)
 
-proc convert_from_binary*(input_binary: string): string =
-  var result_decimal: int = 0
-  var result_hex: string = "reset hex result"
+proc convert_from_binary*(input_binary: string):string =
+  var result_decimal: int = 0; var result_hex:string = "reset hex result"
   for i, c in input_binary:
     #use length to invert i... to get place without a decreasing variable
     if c == '1': result_decimal += 2^abs(i-(input_binary.len-1))
   result_hex = strip(result_decimal.toHex, trailing = false, chars = {'0'})
   return("Decimal is: " & $result_decimal & "\nHexadecimal is: " & result_hex)
 
-proc convert_from_hexadecimal*(input_hex: string): string =
-  var lengthvar_hex: int = input_hex.len-1
-  var result_binary: string = "reset binary result"
-  var result_decimal: int = 0
+proc convert_from_hexadecimal*(input_hex:string):string =
+  var result_binary:string = "reset binary result"; var result_decimal:int = 0
+  var lengthvar_hex:int = input_hex.len-1
   for c in input_hex:
     case c #the number at the end is an offset to the ASCII code
     of '0'..'9': result_decimal += (16^lengthvar_hex) * (int(c)-48)
@@ -34,9 +30,7 @@ proc convert_from_hexadecimal*(input_hex: string): string =
   result_binary = strip(result_decimal.toBin(63), leading = true, trailing = false, chars = {'0'})
   return("Decimal is: " & $result_decimal & "\nBinary is: " & result_binary)
 
-proc benchmark*(): string =
-  var t1: float = cpuTime()
-  var bench_sum: int64 = 0
+proc benchmark*():string =
+  var t1: float = cpuTime(); var bench_sum: int64 = 0
   for n in 0..<100000000: bench_sum+=n #the lessthan sign is to match GDscript output sum
-  var t2: float = cpuTime()
-  return($("Sum is " & $bench_sum & "\n" & "Time taken: " & $(t2-t1) & "s? cpu time"))
+  var t2: float = cpuTime(); return($("Sum is " & $bench_sum & "\n" & "Time taken: " & $(t2-t1) & "s? cpu time"))
