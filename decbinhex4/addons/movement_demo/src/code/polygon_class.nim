7602866b664e672f9314e5605a47250710f09b31
insomniac_lemon <insomniac.lemon@gmail.com>
2024-07-31 03:40:29 -0400

    small changes to polygon_class.nim

--- decbinhex4/addons/movement_demo/src/code/polygon_class.nim
+++ decbinhex4/addons/movement_demo/src/code/polygon_class.nim
@@ -7,15 +7,17 @@ type PolygonClass* = ref object of Polygon2D
 
 method ready*(self:PolygonClass) {.gdsync.} =
   if isRunningInEditor: return
-  print("this will print even if the class is hidden, and esc will still exit")
+  print("prints even w/PolygonClass hidden, process still runs(pres esc to exit)")
   self.Input = /Input
   self.InputMap = /InputMap
 
-#tank movement
+#tank controls
 method process*(self:PolygonClass, delta: float64) {.gdsync.} =
   if isRunningInEditor: return
   if self.Input.isActionPressed("ui_up"): self.moveLocalY(-175.0'f64 * delta)
   elif self.Input.isActionPressed("ui_down"): self.moveLocalY(+155.0'f64 * delta)
-  if self.Input.isActionPressed("ui_right"): self.setRotation(self.getRotation + (8.0'f64 * delta))
-  elif self.Input.isActionPressed("ui_left"): self.setRotation(self.getRotation - (8.0'f64 * delta))
+  if self.Input.isActionPressed("ui_right"):
+    self.setRotation(self.getRotation + (8.0'f64 * delta))
+  elif self.Input.isActionPressed("ui_left"):
+    self.setRotation(self.getRotation - (8.0'f64 * delta))
   if self.Input.isActionPressed "ui_cancel": self.getTree.quit()
